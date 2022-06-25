83ec67a81e775ee0951b7a6d2202a11a4f9ae43a
Jose Garzon <jose.garzon0@misena.edu.co>
2022-06-24 22:18:44 -0500

    :art: structure created to phaser game

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const config ={
+    title: "snake",
+    width: 320,
+    height: 180,
+    type: Phaser.AUTO,
+    parent: "container",
+    backgroundColor: "#f9ca24",
+    pixelArt: true,
+};
+new Phaser.Game(config);
