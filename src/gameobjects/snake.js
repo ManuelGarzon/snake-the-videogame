3c6d35161775cb59f54cc2153d7c121a875fae7e
Jose Garzon <jose.garzon0@misena.edu.co>
2022-06-25 01:49:21 -0500

    keyboard

--- src/gameobjects/snake.js
+++ src/gameobjects/snake.js
@@ -1,10 +1,36 @@
 
 class Snake {
     
-        constructor(scene) {
+     constructor(scene) {
               this.scene = scene;
-              this.scene.physics.add.image(10,10, 'cuerpo');
+              this.cuerpo=[this.scene.physics.add.image(10,10, 'cuerpo')];
+              this.dir = 'derecha';
+              this.timmer = 0;
         }
+
+        changeMov(dir) {
+            this.dir = dir;
+        }
+
+     update(time) {
+        if(time > this.timmer){
+         switch(this.dir){
+             case 'derecha':
+                this.cuerpo[0].x += 10;
+                break;
+            case 'izquierda':
+                this.cuerpo[0].x -= 10;
+                break;
+            case 'arriba':
+                this.cuerpo[0].y -= 10;
+                break;
+            case 'abajo':
+                this.cuerpo[0].y += 10;
+                break;
+            }
+        this.timmer = time + 150;
+        }
+    }
     
 }
  export default Snake
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -1,4 +1,4 @@
-import snake from '../gameobjects/snake.js'
+import Snake from '../gameobjects/snake.js'
 class Play extends Phaser.Scene {
     constructor() {
         super('Play');
@@ -7,7 +7,25 @@ class Play extends Phaser.Scene {
   
     preload() {
         console.log('soy Play');
-        this.snake = new snake (this);
+        this.snake = new Snake(this);
+    }
+    create() {
+        this.input.keyboard.on('keydown-RIGHT', () =>  {
+           this.snake.changeMov('derecha')
+        });
+        this.input.keyboard.on('keydown-LEFT', () => {
+            this.snake.changeMov('izquierda')
+        });
+        this.input.keyboard.on('keydown-UP', () => {
+            this.snake.changeMov('arriba')
+        });
+        this.input.keyboard.on('keydown-DOWN', () => {
+            this.snake.changeMov('abajo')
+        });
+    }
+    update(time) {
+        
+        this.snake.update(time)
     }
 }
 
